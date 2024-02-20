737c3f1d07b6890af8ec68ca28eaab778075355d
Dulguun.p <dulguun.p@mobicom.mn>
2024-02-20 17:34:15 +0800

    secondary color nemsen

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,7 +14,8 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
-        "primary": "#2b891c"
+        "primary": "#2b891c",
+        "secondary": "#CCCCCC"
       }
     },
   },
