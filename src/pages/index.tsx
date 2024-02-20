03a964413a03290e2b5d49a5f5cef9bbddcda77a
xvclenwn <144425999+xvclenwn@users.noreply.github.com>
2024-02-20 17:57:32 +0800

    From New Branch

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,15 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { redirect } from "next/navigation";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <div className="w-screen h-[80vh] bg-white flex flex-row">
-      <div className="w-3/5 h-full bg-green-500"></div>
+      <div className="w-3/5 h-full bg-green-500">
+        <text>Product</text>
+      </div>
       <div className="w-2/5 h-full bg-black"></div>
     </div>
   );
