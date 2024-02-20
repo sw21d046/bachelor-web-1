a76c57ccac8c6ed29031e040aceef96c5b1e9928
xvclenwn <sw21d046@mandakh.edu.mn>
2024-02-20 18:35:46 +0800

    div nemsen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,9 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <div className="w-screen h-[80vh] bg-white flex flex-row">
-      <div className="w-3/5 h-full bg-green-500"></div>
+      <div className="w-3/5 h-full bg-green-500">
+       <div></div>
+      </div>
       <div className="w-2/5 h-full bg-black">
         <p className="text-softGreen">from the daylight</p>
       </div>
